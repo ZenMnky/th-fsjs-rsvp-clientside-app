7a3f036fab23d569ef7f7c32c174132b0483317c
Justin Hager <coachjustinhager@gmail.com>
2020-03-26 11:05:09 -0700

    refactor Build Guest-Entry functions

--- app.js
+++ app.js
@@ -1,147 +1,163 @@
-/**
- * caputre input from form field
- */
+document.addEventListener('DOMContentLoaded', () => {
+    /**
+     * caputre input from form field
+     */
 
-const inviteeForm = document.querySelector('#registrar');
-const newGuestInput = document.querySelector('.registarar__input__name');
-const mainDiv = document.querySelector('div.main');
-const guestList = document.querySelector('#invitedList');
+    const inviteeForm = document.querySelector('#registrar');
+    const newGuestInput = document.querySelector('.registarar__input__name');
+    const mainDiv = document.querySelector('div.main');
+    const guestList = document.querySelector('#invitedList');
 
-/**
- * Create a section for the guest list filter
- */
+    /**
+     * Create a section for the guest list filter
+     */
 
-const div = document.createElement('div');
-const filterCheckBox = document.createElement('input');
-filterCheckBox.type = 'Checkbox';
-const filterLabel = document.createElement('label');
-filterLabel.textContent = `Hide those who haven't reponded`;
+    const div = document.createElement('div');
+    const filterCheckBox = document.createElement('input');
+    filterCheckBox.type = 'Checkbox';
+    const filterLabel = document.createElement('label');
+    filterLabel.textContent = `Hide those who haven't reponded`;
 
-div.appendChild(filterCheckBox);
-div.appendChild(filterLabel);
-mainDiv.insertBefore(div, guestList );
+    div.appendChild(filterCheckBox);
+    div.appendChild(filterLabel);
+    mainDiv.insertBefore(div, guestList );
 
-/**
- * When selected, filter unconfirmed guests
- */
-filterCheckBox.addEventListener('change', (e)=>{
-    const isChecked = e.target.checked;
-    const lis = guestList.children;
-    if (isChecked){
-        //hide unconfirmed guests
-        for (let i = 0; i < lis.length; i += 1){
-            let li = lis[i];
-            if (li.className === 'responded') {
+    /**
+     * When selected, filter unconfirmed guests
+     */
+    filterCheckBox.addEventListener('change', (e)=>{
+        const isChecked = e.target.checked;
+        const lis = guestList.children;
+        if (isChecked){
+            //hide unconfirmed guests
+            for (let i = 0; i < lis.length; i += 1){
+                let li = lis[i];
+                if (li.className === 'responded') {
+                    li.style.display = '';
+                } else {
+                    li.style.display = 'none';
+                }
+            }
+        } else {
+            //show all guests, including unconfirmed guests
+            for (let i = 0; i < lis.length; i += 1){
+                let li = lis[i];
                 li.style.display = '';
-            } else {
-                li.style.display = 'none';
             }
         }
-    } else {
-        //show all guests, including unconfirmed guests
-        for (let i = 0; i < lis.length; i += 1){
-            let li = lis[i];
-            li.style.display = '';
+    });
+
+    /**
+     * Build Guest-Entry function(s)
+     */
+    function buildGuestListEntry(newGuest) {
+        
+        /**
+         * createElement fuction
+         */
+        function createElement(elementName, propertyName, propertyValue, assignClass){
+            const element = document.createElement(elementName);
+            element[propertyName] = propertyValue;
+            if (assignClass != ''){
+                element.className = assignClass;
+            }
+            return element;
         }
+
+        /**
+         * Function to append to Guest List
+         */
+        function appendToGuestList(elementName, propertyName, propertyValue, assignClass){
+            const element = createElement(elementName, propertyName, propertyValue, assignClass);
+            guestListEntry.appendChild(element);
+        }
+        
+        //Build a new guest list entry
+        const guestListEntry = document.createElement('li');
+        const span = appendToGuestList('span', 'textContent', newGuest, '');
+        
+        
+        ////Add a 'Confirmed' label with corresponding checkbox
+        const confirmLabel = createElement('label', 'textContent', 'Confirmed', '');
+                        
+        const confirmCheckbox = createElement('input', 'type', 'Checkbox', '');
+        confirmLabel.appendChild(confirmCheckbox);
+        guestListEntry.appendChild(confirmLabel);
+        
+
+        ////Option to edit guest list entry
+        const editGuestButton = appendToGuestList('button', 'textContent', 'Edit', 'guestListEntry__editGuestButton');
+                
+        ////Option to remove guest from guest list with the click of a button
+        const removeGuestButton = appendToGuestList('button', 'textContent', 'Remove Guest', 'guestListEntry__removeGuestButton');
+        
+
+        return guestListEntry;
     }
-});
 
-/**
- * Build guest entry function(s)
- */
-function buildGuestListEntry(newGuest) {
-    //Build a new guest list entry
-    const guestListEntry = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = newGuest;
-    guestListEntry.appendChild(span);
+    /**
+     * add guest to the guest list
+     */
+    inviteeForm.addEventListener('submit', (e) => {
+        //prevent form from refresing when submitted
+        e.preventDefault();
+        
+        //capture name of new guest, then reset input field
+        const newGuest = newGuestInput.value;
+        newGuestInput.value = '';
+        
+        //build new guest list entry
+        const guestListEntry = buildGuestListEntry(newGuest);
     
-    ////Add a 'Confirmed' label with corresponding checkbox
-    const confirmLabel = document.createElement('label');
-    confirmLabel.textContent = 'Confirmed';
-    
-    const confirmCheckbox = document.createElement('input');
-    confirmCheckbox.type = 'Checkbox';
-    confirmLabel.appendChild(confirmCheckbox);
-    guestListEntry.appendChild(confirmLabel);
+        //add to the existing guest list :)
+        guestList.appendChild(guestListEntry);
+    });
 
-    ////Option to edit guest list entry
-    const editGuestButton = document.createElement('button');
-    editGuestButton.textContent = 'Edit';
-    editGuestButton.className = 'guestListEntry__editGuestButton';
-    guestListEntry.appendChild(editGuestButton);
-    
-    ////Option to remove guest from guest list with the click of a button
-    const removeGuestButton = document.createElement('button');
-    removeGuestButton.textContent = 'Remove Guest';
-    removeGuestButton.className = 'guestListEntry__removeGuestButton';
-    guestListEntry.appendChild(removeGuestButton);
+    /**
+     * Change the style of the guest list entry when status is confirmed
+     */
+    guestList.addEventListener('change', (e) =>{
+        const checkBox = e.target;
+        const checked = checkBox.checked;
+        const guestListEntry = checkBox.parentNode.parentNode;
+        
+        if (checked) {
+            guestListEntry.className = 'responded';
+        } else {
+            guestListEntry.className = '';
+        }
+    });
 
-    return guestListEntry;
-}
-
-/**
- * add guest to the guest list
- */
-inviteeForm.addEventListener('submit', (e) => {
-    //prevent form from refresing when submitted
-    e.preventDefault();
-    
-    //capture name of new guest, then reset input field
-    const newGuest = newGuestInput.value;
-    newGuestInput.value = '';
-    
-    //build new guest list entry
-    const guestListEntry = buildGuestListEntry(newGuest);
-   
-    //add to the existing guest list :)
-    guestList.appendChild(guestListEntry);
-});
-
-/**
- * Change the style of the guest list entry when status is confirmed
- */
-guestList.addEventListener('change', (e) =>{
-    const checkBox = e.target;
-    const checked = checkBox.checked;
-    const guestListEntry = checkBox.parentNode.parentNode;
-    
-    if (checked) {
-        guestListEntry.className = 'responded';
-    } else {
-        guestListEntry.className = '';
-    }
-});
-
-guestList.addEventListener('click', (e) =>{
-    const button = e.target;
-    const li = e.target.parentNode;
-
-    //remove guest
-    if (button.className === 'guestListEntry__removeGuestButton'){
+    guestList.addEventListener('click', (e) =>{
+        const button = e.target;
         const li = e.target.parentNode;
-        const ul = li.parentNode;
-        ul.removeChild(li);
-    } 
-    //edit guest 
-    else if (button.className === 'guestListEntry__editGuestButton') {
-        const span = li.firstElementChild;
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = span.textContent;
-        li.insertBefore(input, span);
-        li.removeChild(span);
-        button.textContent = 'Save';
-        button.className = 'guestListEntry__editGuestButton--save';
-    }
-    //save updated guest name
-    else if (button.className === 'guestListEntry__editGuestButton--save'){
-        const input = li.firstElementChild;
-        const span = document.createElement('span');
-        span.textContent = input.value;
-        li.insertBefore(span, input);
-        li.removeChild(input);
-        button.textContent = 'Edit';
-        button.className = 'guestListEntry__editGuestButton';
-    }
+
+        //remove guest
+        if (button.className === 'guestListEntry__removeGuestButton'){
+            const li = e.target.parentNode;
+            const ul = li.parentNode;
+            ul.removeChild(li);
+        } 
+        //edit guest 
+        else if (button.className === 'guestListEntry__editGuestButton') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'Save';
+            button.className = 'guestListEntry__editGuestButton--save';
+        }
+        //save updated guest name
+        else if (button.className === 'guestListEntry__editGuestButton--save'){
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'Edit';
+            button.className = 'guestListEntry__editGuestButton';
+        }
+    });
 });
